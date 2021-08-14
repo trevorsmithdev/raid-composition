0af7b8dbadf184d432c029feffc1fb000dfc04c2
trevorsmithdev <trevor.smith.dev@gmail.com>
2021-08-13 22:59:00 -0500

    setup grid layouts for classes and lineup

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
-  <Main />
+  <main>
+    <header>
+      <h2>Raid Composition</h2>
+    </header>
+    <div class="main-content">
+      <section class="class-grid-container">
+        <div class="grid-item">1</div>
+        <div class="grid-item">2</div>
+        <div class="grid-item">3</div>
+        <div class="grid-item">4</div>
+        <div class="grid-item">5</div>
+        <div class="grid-item">6</div>
+        <div class="grid-item">7</div>
+        <div class="grid-item">8</div>
+        <div class="grid-item">9</div>
+      </section>
+      <RaidLineup />
+    </div>
+  </main>
 </template>
 
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import Main from './components/Main.vue'
+import RaidLineup from './components/RaidLineup.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
@@ -18,6 +35,31 @@ import Main from './components/Main.vue'
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.main-content {
+  max-width: 1200px;
+  margin: auto;
+}
+
+header {
+  background-color: #5D576B;
+  color: #F4F1BB;
+  padding: 1rem;
+}
+
+.class-grid-container {
+  padding: 1rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-auto-rows: 40px;
+  gap: 1em;
+}
+
+.grid-item {
+  background-color: #E6EBE0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/RaidLineup.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="raid-lineup-container"> 
+    <div class="group-1 group">Group 1</div>
+    <div class="group-2 group">Group 2</div>
+    <div class="group-3 group">Group 3</div>
+    <div class="group-4 group">Group 4</div>
+    <div class="group-5 group">Group 5</div>
+    <div class="group-standby group">Standby</div>
+  </section>
+</template>
+
+<style scoped>
+.raid-lineup-container {
+  background-color: #E6EBE0;
+  padding: 1rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
+  grid-auto-rows: 240px;
+  gap: 1em;
+}
+
+.group {
+  width: 100%;
+  background-color: #5D576B;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #E6EBE0;
+}
+</style>
