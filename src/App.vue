c6022be905644d4cc85a101a65a0cf98403ce4d1
trevorsmithdev <trevor.smith.dev@gmail.com>
2021-08-15 11:18:14 -0500

    add druid spec icons

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,7 @@
       <h2>Raid Composition</h2>
     </header>
     <div class="main-content">
-      <section class="class-grid-container">
-        <div class="grid-item">1</div>
-        <div class="grid-item">2</div>
-        <div class="grid-item">3</div>
-        <div class="grid-item">4</div>
-        <div class="grid-item">5</div>
-        <div class="grid-item">6</div>
-        <div class="grid-item">7</div>
-        <div class="grid-item">8</div>
-        <div class="grid-item">9</div>
-      </section>
+      <ClassSpecSelector />
       <RaidLineup />
     </div>
   </main>
@@ -24,6 +14,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 import Main from './components/Main.vue'
 import RaidLineup from './components/RaidLineup.vue'
+import ClassSpecSelector from './components/ClassSpecSelector.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
@@ -46,20 +37,5 @@ header {
   background-color: #5D576B;
   color: #F4F1BB;
   padding: 1rem;
-}
-
-.class-grid-container {
-  padding: 1rem;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  grid-auto-rows: 40px;
-  gap: 1em;
-}
-
-.grid-item {
-  background-color: #E6EBE0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 }
 </style>
---
+++ src/components/ClassSpecSelector.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="class-grid-container">
+    <div class="grid-item druid">
+      <div class="druid-balance spec-icon" />
+      <div class="druid-feral spec-icon" />
+      <div class="druid-resto spec-icon" />
+    </div>
+    <div class="grid-item">2</div>
+    <div class="grid-item">3</div>
+    <div class="grid-item">4</div>
+    <div class="grid-item">5</div>
+    <div class="grid-item">6</div>
+    <div class="grid-item">7</div>
+    <div class="grid-item">8</div>
+    <div class="grid-item">9</div>
+  </section>
+</template>
+
+<style scoped>
+.class-grid-container {
+  padding: 1rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-auto-rows: 40px;
+  gap: 1em;
+}
+
+.grid-item {
+  background-color: #E6EBE0;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.spec-icon {
+  width: 30px;
+  height: 30px;
+  background-size: cover;
+}
+
+.druid {
+  background-color: rgba(255, 124, 10, .40)
+}
+
+.druid-balance {
+  background-image: url('/src/assets/spec_icons/druid_balance.jpeg');
+}
+
+.druid-feral {
+  background-image: url('/src/assets/spec_icons/druid_feral.jpg');
+}
+
+.druid-resto {
+  background-image: url('src/assets/spec_icons/druid_resto.jpg');
+}
+</style>
